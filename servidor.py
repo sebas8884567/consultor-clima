6c3ae43aa5d7f0134fccdfc6df9137b6199ccc46
sebastian <sebasdelavega66@gmail.com>
2024-09-26 13:52:14 -0500

    actualizacion de codigo con descripcion de codigo

--- servidor.py
+++ servidor.py
@@ -14,10 +14,12 @@ cache_expiration_time = 300  # 5 minutos
 
 # Servidor HTTP que maneja solicitudes GET
 class WeatherRequestHandler(http.server.SimpleHTTPRequestHandler):
+    # Manejo de solicitudes GET
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
         query = urlparse.parse_qs(parsed_path.query)
 
+        # Ruta para la solicitud de clima
         if parsed_path.path == '/weather':
             city = query.get('city', [''])[0]
             if city:
@@ -28,11 +30,12 @@ class WeatherRequestHandler(http.server.SimpleHTTPRequestHandler):
                     print(f"Datos en caché para {city}.")
                     response_data = weather_cache[city]['data']
                 else:
-                    api_key = 'f33412b3d42fc6ddac7aeb0e703b960e'  # Reemplaza con tu clave real
+                    api_key = 'f33412b3d42fc6ddac7aeb0e703b960e'  # clave de la api
                     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=es'
                     
                     response = requests.get(url)
 
+                    # Verificación de la respuesta de la API
                     if response.status_code == 200:
                         weather_data = response.json()
                         temperature = weather_data['main']['temp']
@@ -56,11 +59,13 @@ class WeatherRequestHandler(http.server.SimpleHTTPRequestHandler):
                         self.wfile.write(b'{"error": "Ciudad no encontrada"}')
                         return
 
+                # Respuesta exitosa
                 self.send_response(200)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
                 self.wfile.write(json.dumps(response_data).encode())
             else:
+                # Error por falta de ciudad en la consulta
                 self.send_response(400)
                 self.end_headers()
                 self.wfile.write(b'{"error": "Falta la ciudad en la consulta"}')
